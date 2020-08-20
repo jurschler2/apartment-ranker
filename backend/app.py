ee6bf776a2368515d77c18db1ea750a557b2e76c
Joseph Urschler <jmuwf4@mail.missouri.edu>
2020-08-19 20:37:48 -0700

    added a PATCH route for an apartment, need to add forms for validation

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
 """ Routes to show apartments """
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 # from flask_debugtoolbar import DebugToolBarExtension
 from helpers import get_apartment
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -23,6 +24,8 @@ db = SQLAlchemy(app)
 db.app = app
 db.init_app(app)
 
+from models import Apartment
+
 @app.route('/apartment', methods=["GET"])
 def show_apartment():
     """ Return JSON object of specific apartment """
@@ -33,6 +36,16 @@ def show_apartment():
 
     return output
 
+@app.route('/apartment/<int:apartment_id>', methods=["GET"])
+def show_specific_apartment(apartment_id):
+    """ Return JSON object of specific apartment """
+
+    apt = Apartment.query.get_or_404(apartment_id)
+
+    output = apt.serialize()
+
+    return output  
+
 @app.route('/apartment/<int:apartment_id>', methods=["PATCH"])
 def update_rankings(apartment_id):
     """ PATCH an existing apartment's rankings """
@@ -40,3 +53,12 @@ def update_rankings(apartment_id):
     apt = Apartment.query.get_or_404(apartment_id)
 
     updates = request.json
+
+    for key in updates:
+        setattr(apt, key, updates[key])
+
+    db.session.commit()
+
+    output = apt.serialize()
+
+    return output
--- backend/seed.py
+++ backend/seed.py
@@ -4,3 +4,9 @@ from models import Apartment
 
 db.drop_all()
 db.create_all()
+
+apartment = Apartment(apartment_url='craigslist.org/example',
+                      apartment_address='Masonic near Fell')
+
+db.session.add(apartment)
+db.session.commit()
